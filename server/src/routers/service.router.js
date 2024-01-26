f78854bac232d7baedac65016c5f12abba63a4d9
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2024-01-26 11:08:08 +0530

    Remove jwtVerify middleware from service router

--- server/src/routers/service.router.js
+++ server/src/routers/service.router.js
@@ -4,9 +4,7 @@ const router = Router();
 
 import {createService, getServices,} from '../controllers/service.controller.js';
 
-import {jwtVerify} from '../middleware/authJwtVerify.middleware.js';
 
-router.use(jwtVerify);
 
 router.route('/createService').post(createService)
 router.route('/getServices').get(getServices)
