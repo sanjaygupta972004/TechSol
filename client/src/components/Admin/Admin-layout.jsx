46b64d96213103b781686b192f232413ef173bbe
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2024-01-28 23:38:28 +0530

    Update Admin Dashboard heading

--- client/src/components/Admin/Admin-layout.jsx
+++ client/src/components/Admin/Admin-layout.jsx
@@ -8,7 +8,7 @@ const AdminLayout = () => {
     <div className="flex h-screen mt-2 gap-2">
        
       <div className="w-1/4 bg-gray-800 text-white p-1">
-      <h1 className="text-2xl font-bold mb-4 text-center">Admin Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Admin  Dashboard</h1>
         <AdminNav />
       </div>
       <Outlet/>
